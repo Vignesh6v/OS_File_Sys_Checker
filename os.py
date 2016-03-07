d268ba8ae117f45dab43f584992b7e65546830fc
vignesh6v <vignesh.6v@gmail.com>
2016-03-07 15:16:06 -0500

    Updating Time check function

--- os.py
+++ os.py
@@ -4,15 +4,14 @@ from time import time
 freestart, freeend,ROOT_BLOCK, MAX_BLOCK_NUM  = 0,0,0,0
 DevId = 20
 blocksize = 4096
-current_time = int(time())
 rootstr_blck = str(ROOT_BLOCK)
 root_path = 'FS/fusedata.' + rootstr_blck
 default_path = 'FS/fusedata.'
 superblock_path = 'FS/fusedata.0'
-used_block_list = [ROOT_BLOCK]
+usedblock_list = [ROOT_BLOCK]
 file_data_block_list = []
 new_list, block_list = [], []
-freeblock_list = []
+freeblock_list, notfullblocks = [], []
 free_temp_list = []
 
 
@@ -55,6 +54,7 @@ def check_DevId(file):
         print ('Unable to Open the File')
 
 def get_freeblock_list():
+    global freeblock_list
     for i in range(freestart, freeend+1):
         try: 
             f = open(default_path+str(i), 'r')
@@ -63,17 +63,94 @@ def get_freeblock_list():
             block_list = items.split(',')
             new_list = [j.strip('[') for j in block_list]
             new_list = [j.strip(']') for j in new_list]
+            if len(new_list) < 400:
+                notfullblocks.append(i)
             for k in new_list:
                 freeblock_list.append(k)
         except:
             print ('Unable to Open the File')
     print ('Length of freeblocks list: {}'.format(len(freeblock_list)))
-        
 
+def get_usedblock_list():
+    pass
+        
+def validate_freeblock_list(f,u):
+    value = ''
+    notlisted_block = []
+    totalblock_list = [str(i) for i in range(ROOT_BLOCK, MAX_BLOCK_NUM)]
+    freeblocks_list = list(set(totalblock_list).difference(u))
+    for i in freeblocks_list:
+        if i not in f:
+            notlisted_block.append(i)                         
+    if len(notlisted_block):
+        print('Error!, The free block list does not contains ALL of the free blocks')
+        print ('The blocks that are not listed are: {}'. format(notlisted_block))
+        if len(notfullblocks):
+            file = open(default_path + str(notfullblocks.pop(0)),  'r' )
+            items = file.read()
+            block_list = items.split(',')
+            new_list = [j.strip('[') for j in block_list]
+            new_list = [j.strip(']') for j in new_list]
+            if len(new_list) + len(notlisted_block) <= 400:
+                for i in notlisted_block:
+                    new_list.append(i)
+                #file.write(value)
+                file.close()
+            else:
+                print ('unable to append the free list in single block')
+
+                
+    else:
+        print('The free block list contains ALL of the free blocks')
+        
+    for i in u:
+        if i in f:
+            print('Error!, The Block {} is actually used but its available in Free list Blocks'.format(i))
+        else:
+            print('There are no files/directories stored on items listed in the free block list')
+            
+def check_Time(l):
+    for i in l:
+        f = open(default_path+str(i), 'r')
+        flag = True
+        item = f.read()
+        f.close()
+        atime_stp = item.find('"atime":')
+        atime_edp = item.find (',',atime_stp)
+        print (item[atime_stp:atime_edp])
+        atime_value = item[atime_stp+8:atime_edp]
+        print (atime_value)
+        if int(atime_value) < int(time()):
+            atime_value = int(time())
+            flag = False
+
+        ctime_stp = item.find('"ctime":')
+        ctime_edp = item.find (',',ctime_stp)
+        ctime_value = item[ctime_stp+8:ctime_edp]
+        if int(ctime_value) < int(time()):
+            ctime_value = int(time())
+            flag = False
+
+        mtime_stp = item.find('"mtime":')
+        mtime_edp = item.find (',',mtime_stp)
+        mtime_value = item[mtime_stp+8:mtime_edp]
+        if int(mtime_value) < int(time()):
+            mtime_value = int(time())
+            flag = False
+        
+        if flag == False:
+            f = open(default_path+str(i), 'w+')
+            print ('Updating the Time for Block Num: {}'.format(i))
+            chngd_item = item[:atime_stp+8]+str(atime_value)+item[atime_edp:ctime_stp+8]+str(ctime_value)+item[ctime_edp:mtime_stp+8]+str(mtime_value)+item[mtime_edp:]
+            f.write(chngd_item)
+ 
 def main():
     print ('Started')
-    check_DevId(superblock_path)
-    get_freeblock_list()
+#    check_DevId(superblock_path)
+#    get_freeblock_list()
+#    get_usedblock_list()
+ #   validate_freeblock_list(freeblock_list, usedblock_list)
+    check_Time([27])
 
 
 if __name__ == '__main__':main()
